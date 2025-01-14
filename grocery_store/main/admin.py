3897654997e4cbd4d389a7483c00a147a205292d
daheeahn <deg9810@gmail.com>
2025-01-14 15:20:51 -0800

    fix comments

--- grocery_store/main/admin.py
+++ grocery_store/main/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from .models import User, Product, Basket, PurchaseHistory
 
-# 사용자 모델 등록
+# register user model
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'user_type')
 
-# 제품 모델 등록
+# register product model
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price')
 
-# 장바구니 모델 등록
+# register basket model
 @admin.register(Basket)
 class BasketAdmin(admin.ModelAdmin):
     list_display = ('customer', 'status', 'created_at')
 
-# 구매 이력 모델 등록
+# register purchase history model
 @admin.register(PurchaseHistory)
 class PurchaseHistoryAdmin(admin.ModelAdmin):
     list_display = ('customer', 'get_products', 'purchased_at')
